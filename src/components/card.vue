68a9f58e5fe093d496b3a6ca383ab790461be5d9
john-mirage <chaosdynamix@posteo.net>
2022-06-10 16:48:26 +0200

    fix: fix types

--- src/components/card.vue
+++ src/components/card.vue
@@ -16,12 +16,12 @@ function closePopover() {
 
 <template>
   <main class="relative w-full max-w-card mx-auto bg-white rounded-card overflow-hidden shadow-xl text-center sm:text-left lg:max-w-card-lg lg:flex lg:flex-row lg:overflow-visible">
-    <img class="flex-grow-0 flex-shrink-0 basis-auto lg:w-285 lg:object-cover lg:object-left lg:rounded-r-none lg:rounded-l-card" src="/src/images/drawers.jpg" alt="">
+    <img draggable="false" class="flex-grow-0 flex-shrink-0 basis-auto lg:w-285 lg:object-cover lg:object-left lg:rounded-r-none lg:rounded-l-card" src="/src/images/drawers.jpg" alt="">
     <div class="pt-36 px-32 pb-20 lg:flex-1 lg:px-40 lg:py-32">
       <p class="text-16 font-bold text-very-dark-grayish-blue tracking-02 mb-16 lg:text-20">Shift the overall look and feel by adding these wonderful touches to furniture in your home</p>
       <p class="text-13 font-medium text-desaturated-dark-blue tracking-01 mb-32">Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I've got some simple tips to help you make any room feel complete.</p>
       <div class="flex flex-col items-center space-y-16 sm:space-y-0 sm:flex-row">
-        <img class="flex-grow-0 flex-shrink-0 basis-auto w-40 h-40 rounded-full sm:mr-16" src="/src/images/avatar-michelle.jpg" alt="">
+        <img draggable="false" class="flex-grow-0 flex-shrink-0 basis-auto w-40 h-40 rounded-full sm:mr-16" src="/src/images/avatar-michelle.jpg" alt="">
         <div class="flex-1">
           <p class="text-13 font-bold text-very-dark-grayish-blue tracking-01">Michelle Appleton</p>
           <p class="text-13 font-medium text-grayish-blue tracking-01">28 Jun 2020</p>
