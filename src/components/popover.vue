6ff586ba2c7dad41ca6b0a6b42f944f6b9f356f8
john-mirage <chaosdynamix@posteo.net>
2022-06-10 17:11:49 +0200

    feat: deploy

--- src/components/popover.vue
+++ src/components/popover.vue
@@ -48,7 +48,7 @@ onUnmounted(() => {
       </ul>
     </nav>
     <div class="flex-1 flex flex-row justify-end lg:hidden">
-      <button @click="$emit('closePopover')" class="flex-grow-0 flex-shrink-0 basis-auto flex justify-center items-center w-32 h-32 rounded-full bg-desaturated-dark-blue text-white">
+      <button aria-label="close popover" @click="$emit('closePopover')" class="flex-grow-0 flex-shrink-0 basis-auto flex justify-center items-center w-32 h-32 rounded-full bg-desaturated-dark-blue text-white">
         <svg class="w-15 h-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 13">
           <path fill="currentColor" d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"/>
         </svg>
