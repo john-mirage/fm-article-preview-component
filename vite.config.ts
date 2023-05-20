2bdf99af9b11af1827c3d85f4be56be01faac159
John Mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-20 13:38:06 +0200

    fix: base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/article-preview-component/",
   plugins: [vue()],
   resolve: {
     alias: {
